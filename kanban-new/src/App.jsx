f8393dbf7a6ffc4aa6b16b1aa7735ba2abb1b79f
Almash N <almash050496@gmail.com>
2025-05-11 02:57:10 +0700

    интерфейс отстилизован с помощью Styled Components

--- kanban-new/src/App.jsx
+++ kanban-new/src/App.jsx
@@ -6,10 +6,15 @@ import { PopBrowse } from "./components/popups/popBrowse/PopBrowse";
 import { PopNewCard } from "./components/popups/popNewCard/PopNewCard";
 import { PopUser } from "./components/popups/popUser/PopUser";
 import { cardList } from "../data";
+import { GlobalStyle, Wrapper } from "./Global.styled";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./theme";
+
 
 function App() {
   const [cards, setCards] = useState(cardList);
   const [isLoading, setIsLoading] = useState(false);
+  const [theme, setTheme] = useState("lightTheme");
 
   const addCard = () => {
     const newCard = {
@@ -29,19 +34,20 @@ function App() {
     }, 2000);
   }, []);
   return (
-    <>
-      <div className="wrapper">
+    <ThemeProvider theme={theme === "lightTheme" ? lightTheme : darkTheme}>
+      <GlobalStyle />
+      <Wrapper>
         <PopUser></PopUser>
         <PopNewCard></PopNewCard>
         <PopBrowse></PopBrowse>
-        <Header addCard={addCard}></Header>
+        <Header addCard={addCard} theme={theme} setTheme={setTheme}></Header>
         {isLoading ? (
-          <p className="container loader">Данные загружаются...</p>
+          <p className="loader">Данные загружаются...</p>
         ) : (
           <Main cards={cards}></Main>
         )}
-      </div>
-    </>
+      </Wrapper>
+    </ThemeProvider>
   );
 }
 
--- kanban-new/src/components/main/Main.jsx
+++ kanban-new/src/components/main/Main.jsx
@@ -1,23 +1,24 @@
 import { statusList } from "../../../data";
+import { Container } from "../../Global.styled";
 import { Column } from "../column/Column";
+import * as S from "./Main.styled";
 
-export const Main = ({cards}) => {
-  
+export const Main = ({ cards }) => {
   return (
-    <main className="main">
-      <div className="container">
-        <div className="main__block">
-          <div className="main__content">
+    <S.Main>
+      <Container>
+        <S.MainBlock>
+          <S.MainContent>
             {statusList.map((status, id) => (
-              <Column 
-              key={id}
-              title={status}
-              cards={cards.filter((card) => card.status === status)}
+              <Column
+                key={id}
+                title={status}
+                cards={cards.filter((card) => card.status === status)}
               ></Column>
             ))}
-          </div>
-        </div>
-      </div>
-    </main>
+          </S.MainContent>
+        </S.MainBlock>
+      </Container>
+    </S.Main>
   );
 };
